3d30f940340345480b1cf088960fd385a38e2876
guitarrkurt <guitarrkurt@Cony.local>
2016-06-23 12:42:13 -0500

    Navegacion

---
+++ Assets/escenas/e_arqui/scripts/botonesArqui.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class botonesArqui : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void regresarMenuPrincipal(){
+		// get the current scene name 
+		//string sceneName = SceneManager.GetActiveScene().name;
+
+		// load the same scene
+		SceneManager.LoadScene("menu",LoadSceneMode.Single);
+	}
+}
---
+++ Assets/escenas/e_menu/scripts/navigButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class navigButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ir_escenaArqui(){
+		// get the current scene name 
+		SceneManager.LoadScene("escenaArqui",LoadSceneMode.Single);
+
+	}
+
+	public void exit(){
+		Application.Quit ();
+	}
+}
